1625979f64176fc1525729d35c4865ecd4fcc950
ecostabal <ecostabal@urbex.cl>
2023-12-08 21:06:52 -0300

    testings

--- function.js
+++ function.js
@@ -2,7 +2,6 @@ const axios = require('axios');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const path = require('path');
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
 // Configuración de claves API y URL de API
 const apiKeyMonday = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzMjg3MzUyNCwiYWFpIjoxMSwidWlkIjoyMzUzNzM2NCwiaWFkIjoiMjAyMy0wMS0zMVQyMTowMjoxNy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTUwNzUxNiwicmduIjoidXNlMSJ9.lX1RYu90B2JcH0QxITaF8ymd4d6dBes0FJHPI1mzSRE';
@@ -10,91 +9,41 @@ const urlApi = 'https://api.sandbox.firmex.cloud/v1/signflow/';
 
 async function generateJWT() {
     try {
-      const secretName = 'projects/1096874551601/secrets/jwt-gofirmex/versions/1'; // Reemplaza con el nombre de tu secreto
-      const client = new SecretManagerServiceClient();
-      const [version] = await client.accessSecretVersion({ name: secretName });
-  
-      const privateKey = version.payload.data.toString('utf8');
-      const payload = {
-        user: "ecostabal@urbex.cl",
-        iss: "gimalaga-user",
-        iat: Math.floor(Date.now() / 1000) + (5 * 60)
-      };
-      return jwt.sign(payload, privateKey, { algorithm: 'RS256' });
-    } catch (error) {
-      throw new Error('Error al generar el JWT: ' + error.message);
-    }
-  }
+        // Lee la clave privada desde la URL
+        const privateKeyUrl = 'https://storage.googleapis.com/facturas-urbex/jwt.key';
+        
+        // Realiza una solicitud HTTP para obtener la clave privada desde la URL
+        const response = await axios.get(privateKeyUrl);
 
-  async function getMondayItemData(itemId) {
-    const query = `query { items(ids: [${itemId}]) { column_values { id type value text } } }`;
+        // Obtén el contenido de la clave privada desde la respuesta
+        const privateKey = response.data;
 
-    const response = await axios.post('https://api.monday.com/v2', {
-        query: query
-    }, {
-        headers: {
-            'Authorization': `Bearer ${apiKeyMonday}`, // Aquí se agrega la clave de API
-            'Content-Type': 'application/json'
+        if (!privateKey) {
+            throw new Error('La clave privada no está configurada en las variables de entorno.');
         }
-    });
 
-    return response.data.data.items[0].column_values;
+        const payload = {
+            user: "ecostabal@urbex.cl",
+            iss: "gimalaga-user",
+            iat: Math.floor(Date.now() / 1000) + (5 * 60)
+        };
+
+        return jwt.sign(payload, privateKey, { algorithm: 'RS256' });
+    } catch (error) {
+        throw new Error('Error al generar el JWT: ' + error.message);
+    }
 }
-
 
 // Funciones para interactuar con GoFirmex (crear portafolio, subir documento, etc.)
 
 // Crear portafolio en GoFirmex
-async function createPortfolio(portfolioType, signers, clientPortfolioId = '') {
-    const jwt = await generateJWT(); // Espera a que se genere el JWT
-
-    // Mapeo de selecciones de Monday.com a IDs de GoFirmex
-    const portfolioIds = {
-        'Arrendamiento': '58663eb1-9fe3-4ecd-8662-b405366954d2', // Contrato de Arriendo
-        'Mandato': '2d036dd8-81ed-4c71-89cf-056533032cd2' // Mandato de Administración de Arriendo
-    };
-
-    const portfolioId = portfolioIds[portfolioType]; // Obtén el ID correcto según la selección
-    
-    try {
-        const response = await axios.post(`${urlApi}portfolio`, {
-            portfolio_type: portfolioId,
-            signers: signers, // Debes estructurar esta variable siguiendo el formato necesario
-            client_portfolio_id: clientPortfolioId // Opcional, puede ser un string vacío
-        }, {
-            headers: {
-                'Authorization': `Bearer ${jwt}`,
-                'Content-Type': 'application/json'
-            }
-        });
-
-        return response.data; // Asume que la respuesta incluye los detalles del portafolio creado
-    } catch (error) {
-        console.error('Error creando el portafolio en GoFirmex:', error);
-        throw error;
-    }
-}
-
-// Crear documento
-function encodeFileToBase64(filePath) {
-    return fs.readFileSync(filePath, { encoding: 'base64' });
-}
-
-async function createDocumentInPortfolio(portfolioId, filePath, documentDetails) {
-    const jwt = generateJWT();
-    const documentBase64 = encodeFileToBase64(filePath);
+async function createPortfolio(tipoContratoColumn, signers) {
+    const jwt = await generateJWT();
 
     try {
-        const response = await axios.post(`${urlApi}portfolio_document`, {
-            portfolio_id: portfolioId,
-            document: documentBase64,
-            description: documentDetails.description,
-            client_document_id: documentDetails.clientDocumentId,
-            document_type: documentDetails.documentType,
-            sign: documentDetails.sign,
-            notary_sign: documentDetails.notarySign,
-            document_format: 'pdf',
-            document_hash: documentDetails.documentHash
+        const response = await axios.post(`${urlApi}/portfolio`, {
+            contract_type: tipoContratoColumn,
+            signers: signers
         }, {
             headers: {
                 'Authorization': `Bearer ${jwt}`,
@@ -104,43 +53,45 @@ async function createDocumentInPortfolio(portfolioId, filePath, documentDetails)
 
         return response.data;
     } catch (error) {
-        console.error('Error creando documento en portafolio:', error);
+        console.error('Error creando portafolio en GoFirmex:', error);
         throw error;
     }
 }
 
 // Subir documento a GoFirmex
 async function uploadDocumentToPortfolio(portfolioId, documentFilePath) {
-    const jwt = generateJWT();
-    const formData = new FormData();
-    formData.append('file', fs.createReadStream(documentFilePath));
+    const jwt = await generateJWT();
 
     try {
-        const response = await axios.post(`${urlApi}portfolio/${portfolioId}/document`, formData, {
+        const documentBase64 = encodeFileToBase64(documentFilePath);
+        const response = await axios.post(`${urlApi}/portfolio/${portfolioId}/document`, {
+            document: documentBase64,
+            document_format: 'pdf'
+        }, {
             headers: {
                 'Authorization': `Bearer ${jwt}`,
-                'Content-Type': 'multipart/form-data'
+                'Content-Type': 'application/json'
             }
         });
 
         return response.data;
     } catch (error) {
-        console.error('Error subiendo documento al portafolio:', error);
+        console.error('Error subiendo documento al portafolio en GoFirmex:', error);
         throw error;
     }
 }
 
 // Asignar documentos de identidad
 async function assignIdentityDocument(portfolioId, signerNin, documentPath, documentDetails) {
-    const jwt = await generateJWT(); // Espera a que se genere el JWT
-    const documentBase64 = encodeFileToBase64(documentPath);
+    const jwt = await generateJWT();
 
     try {
-        const response = await axios.post(`${urlApi}portfolio_signer_document`, {
+        const documentBase64 = encodeFileToBase64(documentPath);
+        const response = await axios.post(`${urlApi}/portfolio_signer_document`, {
             portfolio_id: portfolioId,
             document: documentBase64,
+            signer_nin: signerNin,
             description: documentDetails.description,
-            signer_nin: signerNin, // Reemplaza 'signerNin' con el valor correcto
             client_document_id: documentDetails.clientDocumentId,
             document_format: 'pdf',
             document_type: documentDetails.documentType,
@@ -154,17 +105,17 @@ async function assignIdentityDocument(portfolioId, signerNin, documentPath, docu
 
         return response.data;
     } catch (error) {
-        console.error('Error al asignar el documento de identidad:', error);
-        throw new Error('Error al asignar el documento de identidad: ' + error.message);
+        console.error('Error asignando documento de identidad en GoFirmex:', error);
+        throw error;
     }
 }
 
 // Iniciar proceso de firma
 async function initiateSigningProcess(portfolioId) {
-    const jwt = generateJWT();
+    const jwt = await generateJWT();
 
     try {
-        const response = await axios.post(`${urlApi}start_portfolio_signing`, {
+        const response = await axios.post(`${urlApi}/start_portfolio_signing`, {
             portfolio_id: portfolioId
         }, {
             headers: {
@@ -175,7 +126,7 @@ async function initiateSigningProcess(portfolioId) {
 
         return response.data;
     } catch (error) {
-        console.error('Error iniciando el proceso de firma:', error);
+        console.error('Error iniciando el proceso de firma en GoFirmex:', error);
         throw error;
     }
 }
@@ -202,18 +153,6 @@ exports.uploadContract = async (req, res) => {
         const numeroUnidadColumn = columnsData.find(column => column.id === 'texto');
         const archivoContratoColumn = columnsData.find(column => column.id === 'archivo0');
 
-        // Datos de cada firmante
-        const nombreFirmanteColumn = columnsData.find(column => column.id === 'reflejo0');
-        const apellidoFirmanteColumn = columnsData.find(column => column.id === 'reflejo');
-        const rutFirmanteColumn = columnsData.find(column => column.id === 'reflejo_1');
-        const telefonoFirmanteColumn = columnsData.find(column => column.id === 'reflejo_2');
-        const correoFirmanteColumn = columnsData.find(column => column.id === 'reflejo_3');
-
-        // Extracción de datos de documentos de identidad de monday.com
-        const dominioVigente = columnsData.find(column => column.id === 'archivo');
-        const versoCedula = columnsData.find(column => column.id === 'reflejo_12');
-        const reversoCedula = columnsData.find(column => column.id === 'reflejo_14');
-        
         // Define la variable 'signers' y realiza el mapeo de los datos de los firmantes
         const signers = columnsData.map(data => {
             // Asegúrate de mapear los campos correctamente según tu estructura de datos
@@ -227,47 +166,39 @@ exports.uploadContract = async (req, res) => {
                 notification: "none" // O "email" si necesitas enviar notificaciones
             };
         });
-        
-        // Creación del portafolio en GoFirmex y manejo de respuesta
+
+        // Creación del portafolio en GoFirmex
         console.log("Creando el portafolio en GoFirmex");
         const portfolioResponse = await createPortfolio(tipoContratoColumn, signers);
         const portfolioId = portfolioResponse.data.portfolio.id;
         console.log(`Portafolio creado en GoFirmex con ID: ${portfolioId}`);
-        const filePath = '/ruta/al/documento.pdf'; // Reemplaza con la ruta real al archivo
+        const filePath = archivoContratoColumn.value; // Reemplaza con la ruta real al archivo
         const documentDetails = {
-            description: ubicacionPropiedadColumn + numeroUnidadColumn,
-            clientDocumentId: nin,
-            documentType: tipoContratoColumn,
-            sign: true,
-            notarySign: true,
+            description: ubicacionPropiedadColumn.text + numeroUnidadColumn.text,
+            clientDocumentId: tipoContratoColumn.text, // Reemplaza con el valor correcto
+            documentType: tipoContratoColumn.text, // Reemplaza con el valor correcto
             documentHash: "Hash del Documento" // Calcula o define este valor según sea necesario
         };
 
-       // Subir un documento a GoFirmex
-       console.log("Subiendo documento a GoFirmex");
-       const documentFilePath = archivoContratoColumn; // Reemplaza con la ruta real al archivo
-       await uploadDocumentToPortfolio(portfolioId, documentFilePath);
+        // Subir un documento a GoFirmex
+        console.log("Subiendo documento a GoFirmex");
+        await uploadDocumentToPortfolio(portfolioId, filePath);
 
         // Asignación de documentos de identidad y creación de documento en el portafolio
         console.log("Asignando documentos de identidad");
-        await assignIdentityDocument(portfolioId, signerNin, filePathVerso, documentDetails);
-        await assignIdentityDocument(portfolioId, signerNin, filePathReverso, documentDetails);
-        await assignIdentityDocument(portfolioId, signerNin, fileDominioVigente, documentDetails);
+        const filePathVerso = path.join(__dirname, 'archivos', 'versoCedula.pdf'); // Reemplaza con la ruta real al archivo
+        const filePathReverso = path.join(__dirname, 'archivos', 'reversoCedula.pdf'); // Reemplaza con la ruta real al archivo
+        const fileDominioVigente = path.join(__dirname, 'archivos', 'dominioVigente.pdf'); // Reemplaza con la ruta real al archivo
 
-        // Asignar documentos de identidad
-        const filePathVerso = path.join(__dirname, 'archivos', versoCedula);
-        const filePathReverso = path.join(__dirname, 'archivos', reversoCedula);
-        const fileDominioVigente = path.join(__dirname, 'archivos', dominioVigente);
+        await assignIdentityDocument(portfolioId, signers[0].nin, filePathVerso, documentDetails);
+        await assignIdentityDocument(portfolioId, signers[0].nin, filePathReverso, documentDetails);
+        await assignIdentityDocument(portfolioId, signers[0].nin, fileDominioVigente, documentDetails);
 
-        console.log("Creando documento en el portafolio");
-        const documentResponse = await createDocumentInPortfolio(portfolioId, filePath, documentDetails);
-        console.log(`Documento creado en el portafolio con ID: ${documentResponse.documentId}`);
-
+        // Iniciar el proceso de firma
         console.log("Iniciando el proceso de firma");
         const signingResponse = await initiateSigningProcess(portfolioId);
 
         console.log("Item procesado correctamente");
-
         res.status(200).send("Item procesado correctamente");
     } catch (error) {
         console.error("Error en la función:", error.message);
@@ -288,4 +219,8 @@ async function getMondayItemData(itemId) {
     });
 
     return response.data.data.items[0].column_values;
+}
+
+function encodeFileToBase64(filePath) {
+    return fs.readFileSync(filePath, { encoding: 'base64' });
 }
